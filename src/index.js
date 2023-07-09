a6d214ec469f3d7bab2850e3cc32515dc2e5d3cf
hyunin3 <stock4share2@gmail.com>
2023-07-09 21:05:46 +0900

    refine index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// index.js
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
 
-ReactDOM.render(
+ReactDOM.createRoot(
+  document.getElementById('root')
+).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
